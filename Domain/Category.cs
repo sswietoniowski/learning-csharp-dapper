fd09380ebfa289867eb261dc25a1816d7cd26fc4
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-12-21 16:05:01 +0100

    Working with mappings usind db aliases

--- Domain/Category.cs
+++ Domain/Category.cs
@@ -4,13 +4,8 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
+        public string Info { get; set; }
         public bool IsNew => Id == default(int);
         public List<Product> Products { get; } = new List<Product>();
-
-        public override string ToString()
-        {
-            return $"{Id} {Name}";
-        }
     }
 }
